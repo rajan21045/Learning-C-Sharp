d327c37b7072c48bf3a6d3442f55bbfac822f41f
Rajan Poudel <raajan.works@gmail.com>
2026-05-26 21:44:26 +0545

    Add files via upload

---
+++ SELF LEARNING/EXCEPTIONS/exceptions1.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] nums = {1, 3, 4, 5, 2};
+            Console.Write(nums[6]); 
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Something went wrong.");
+        }
+    }
+}
