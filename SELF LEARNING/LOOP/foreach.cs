d7612566000d002db590e92e03c24969f3420e9b
Rajan Poudel <raajan.works@gmail.com>
2026-05-26 21:32:36 +0545

    Add files via upload

---
+++ SELF LEARNING/LOOP/foreach.cs
@@ -0,0 +1,20 @@
+using System;
+class Program{
+    static void Main(string[] args)
+    {
+        /*
+            The foreach Loop
+            There is also a foreach loop, which is used exclusively to loop through elements in an array (or other data sets):
+            Syntax
+            foreach (type variableName in arrayName) 
+            {
+            // code block to be executed
+            }
+        */
+        int[] nums ={1, 2, 3, 4, 5};
+        foreach (int item in nums)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
